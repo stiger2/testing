e51d60260842dc3d0b8a436d6bfe3e27263632d6
stiger <adeshina.shakiru@yahoo.com>
2019-09-09 20:41:49 -0400

    initial commit

---
+++ AdeshinaShop/app/src/main/java/android/bignerdbranch/adeshinashop/MenuActivity.java
@@ -0,0 +1,14 @@
+package android.bignerdbranch.adeshinashop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
